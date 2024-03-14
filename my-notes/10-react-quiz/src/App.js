08d10653de7f61535a62d613c8215092a0d6aaf5
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-15 02:50:48 +0500

    set environment for react quiz app

--- my-notes/10-react-quiz/src/App.js
+++ my-notes/10-react-quiz/src/App.js
@@ -1,9 +1,17 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+      {/* Use That DateCounter to understood the UseReducer Hook in React Js */}
+      {/* <DateCounter /> */}
     </div>
   );
 }
